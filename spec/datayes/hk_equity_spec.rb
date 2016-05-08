60fbcc6051c730f261b6929072ecd622017c98d3
ShallmentMo <ShallmentMo@gmail.com>
2016-05-08 14:36:32 +0800

    add datayes/hk_equity

--- spec/datayes/hk_equity_spec.rb
+++ spec/datayes/hk_equity_spec.rb
@@ -4,4 +4,12 @@ describe Tushare::Datayes do
   before do
     @datayes = Tushare::Datayes
   end
+
+  it 'can get hk_equ data' do
+    tp @datayes.hk_equ(ticker: '00169')
+  end
+
+  # it 'can get hk_equ_ca data' do
+  #   tp @datayes.hk_equ_ca(ticker: '00001')
+  # end
 end
