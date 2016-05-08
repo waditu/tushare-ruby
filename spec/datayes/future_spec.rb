252fcef8726c3c11b23a12c76cbde536d1a2f97e
ShallmentMo <ShallmentMo@gmail.com>
2016-05-08 14:29:07 +0800

    add datayes/future

--- spec/datayes/future_spec.rb
+++ spec/datayes/future_spec.rb
@@ -4,4 +4,12 @@ describe Tushare::Datayes do
   before do
     @datayes = Tushare::Datayes
   end
+
+  it 'can get futu data' do
+    tp @datayes.futu(ticker: 'cu1106')
+  end
+
+  it 'can get futu_convf data' do
+    tp @datayes.futu_convf(ticker: 'TF1503')
+  end
 end
